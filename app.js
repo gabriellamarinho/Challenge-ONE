b402dad40c2d7c30c2c9db1f81fdf89257f5cf3a
gabriellamarinho <gabriellamarilia30@gmail.com>
2025-03-18 13:36:49 -0300

    update project

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
-
 let amigos = [];
 
 function adicionarAmigo() {
-
-  const inputAmigo = document.getElementById("amigo");
-  const nomeDoAmigo = inputAmigo.value.trim().tittle(); 
+  const input = document.getElementById("amigo");
+  const nomeDoAmigo = input.value.trim();
 
   if (nomeDoAmigo === "") {
-    alert("Por favor, insira um nome.");
-    return; 
+    alert("Insira um nome válido.");
+    return;
   }
 
-    amigos.push(nomeDoAmigo);
-    inputAmigo.value = "";
-    atualizarListaDeAmigos();
+
+  amigos.push(nomeDoAmigo);
+  input.value = "";
+  atualizarLista();
+  return;
 }
 
-function atualizarListaDeAmigos() {
+
+function atualizarLista() {
 
   const listaAmigosElement = document.getElementById("listaAmigos");
-
   listaAmigosElement.innerHTML = "";
 
-  for (let i = 0; i < amigos.length; i++) {
-    const amigo = amigos[i];
+  amigos.forEach(amigo => {
     const listItem = document.createElement("li");
     listItem.textContent = amigo;
     listaAmigosElement.appendChild(listItem);
-  }
+
+  });
 }
 
 function sortearAmigo() {
-  
   if (amigos.length === 0) {
     alert("Adicione amigos à lista antes de sortear.");
     return;
@@ -41,23 +40,18 @@ function sortearAmigo() {
   const amigoSorteado = amigos[indiceAleatorio];
   const resultadoElement = document.getElementById("resultado");
   resultadoElement.innerHTML = `<li class="result-item">O amigo secreto sorteado foi: <strong>${amigoSorteado}</strong></li>`;
-
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-
   const botaoAdicionar = document.querySelector(".button-add");
-
   if (botaoAdicionar) {
     botaoAdicionar.addEventListener('click', adicionarAmigo);
   }
 
   const botaoSortear = document.querySelector(".button-draw");
-  
   if (botaoSortear) {
     botaoSortear.addEventListener('click', sortearAmigo);
   }
 
-  atualizarListaDeAmigos();
-
+  atualizarLista();
 });
